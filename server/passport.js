28c36f38cd54cdb24c484a26b1a19025822c1401
John Goyette <jagoyette@gmail.com>
2024-04-09 17:49:11 -0400

    Removed env log

--- server/passport.js
+++ server/passport.js
@@ -10,26 +10,22 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-// Check environment
+// Register Google authentication strategy
 const clientID = process.env.GOOGLE_CLIENT_ID
-if (!clientID) {
-    console.error('GOOGLE_CLIENT_ID not populated');
-}
-
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-if (!clientSecret) {
-    console.error('GOOGLE_CLIENT_SECRET not populated');
+if (clientID && clientSecret) {
+    passport.use(new GoogleStrategy({
+        clientID: clientID,
+        clientSecret: clientSecret,
+        callbackURL: '/auth/google/callback',
+        passReqToCallback: false
+    },
+        // Verify user function
+        async function(accessToken, refreshToken, profile, done) {
+            const user = await UserRepository.findOrCreateUser(profile);
+            return done(null, user?.toObject());
+        }
+    ));
+} else {
+    console.warn('Google Login Strategy not implemented. Make sure CLIENT environment variables are populated.');
 }
-
-passport.use(new GoogleStrategy({
-    clientID: clientID,
-    clientSecret: clientSecret,
-    callbackURL: '/auth/google/callback',
-    passReqToCallback: false
-},
-    // Verify user function
-    async function(accessToken, refreshToken, profile, done) {
-        const user = await UserRepository.findOrCreateUser(profile);
-        return done(null, user?.toObject());
-    }
-));
