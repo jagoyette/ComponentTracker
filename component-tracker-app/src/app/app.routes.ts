a6ffd0c12831656698567c1f31fe478a6bfddeae
John Goyette <jagoyette@gmail.com>
2024-04-09 21:45:34 -0400

    Added login and home components

--- component-tracker-app/src/app/app.routes.ts
+++ component-tracker-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomePageComponent },
+    { path: "home", component: HomePageComponent },
+    { path: "login", component: LoginPageComponent },
+
+    // Wildcards and redirect routes
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+];
